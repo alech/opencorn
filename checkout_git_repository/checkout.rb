50f3c06fd505212e269b1fabc5ec6be9c2ed5a96
Alexander Klink <git@alech.de>
2010-09-19 23:04:08 +0200

    some work on checkout.rb, more debugging, tags are not ordered so iterate over all of them ...

--- checkout_git_repository/checkout.rb
+++ checkout_git_repository/checkout.rb
@@ -35,23 +35,31 @@ g = Git.clone(SOURCE_REPOSITORY, tmpdir)
 
 object_signatures = {}
 most_current_signed_object = nil
+pp g.tags if DEBUG
+# iterate over all tags, as they are not sorted by time
 g.tags.each do |tag|
+    puts "Checking tag #{tag.name}" if DEBUG
     if ! tag.name[/^[a-zA-Z0-9]+$/] then
         puts 'Argh, non-alphanumeric tag name, WTF?'
         exit 1
     end
     object_id = tag.contents_array[0][/object ([a-f0-9]+)/, 1]
+    puts "Refers to object id #{object_id}" if DEBUG
     if valid_signature(tag.name) then
+        puts "Valid signature on #{tag.name} by #{signer_id(tag.name)}" if DEBUG
         object_signatures[object_id] ||= {}
         object_signatures[object_id][signer_id(tag.name)] = 1
-        if object_signatures[object_id].keys.length >= 2 then
-            most_current_signed_object = object_id
-            break
-        end
     end
 end
-
 pp object_signatures if DEBUG
+
+# iterate over all commits to find the first that has more than one signature
+g.log.each do |log|
+    if object_signatures[log.objectish] && object_signatures[log.objectish].keys.size >= 2 then
+        most_current_signed_object = log.objectish
+        break
+    end
+end
 
 if ! most_current_signed_object then
     STDERR.puts "Sorry, no object with more than one signed tag found."
@@ -64,4 +72,5 @@ begin
 FileUtils.rm_r DEST_REPOSITORY, :secure => true
 rescue # ignore errors deleting the directory
 end
-g2 = Git.clone(tmpdir, DEST_REPOSITORY)
+# FIXME :depth seems to be broken
+g2 = Git.clone(tmpdir, DEST_REPOSITORY, :depth => 1)
