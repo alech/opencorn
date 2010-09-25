58d93595ed17b4b04f01d8bb25c820b756b38dba
Alexander Klink <git@alech.de>
2010-09-25 13:54:52 +0200

    init_card: support readers with pinpad (untested)

--- init_card/init_card.rb
+++ init_card/init_card.rb
@@ -6,6 +6,22 @@ require 'opencorn/config'
 require 'rubygems'
 require 'git'
 
+DEBUG = false
+
+def run_tool(toolname, options, backticks = false)
+	if OpenCorn::Config['PINPAD'] then
+		command = ":|#{toolname} -p - #{options}"
+	else
+		command = "#{toolname} #{options}"
+	end
+	puts "running #{command}" if DEBUG
+	if backticks then
+		`#{command}`
+	else
+		system "#{command}"
+	end
+end
+
 # delete card
 print "Are you sure you want to delete the card? "
 answer = STDIN.readline.chomp
@@ -13,7 +29,7 @@ if answer[0,1].downcase != 'y' then
 	exit 1
 end
 
-if ! system "pkcs15-init -E" then
+if ! run_tool("pkcs15-init", "-E") then
 	STDERR.puts "Error running pkcs15-init -E"
 	exit 2
 end
@@ -23,8 +39,7 @@ nick = ""
 print "Please enter your nickname (alphanumeric): "
 nick = STDIN.readline.chomp[/^([a-zA-Z0-9]+)/, 1]
 
-# TODO: PIN/PUK input using pinpad (:|... -p -)
-if ! system "pkcs15-init --create-pkcs15 --profile pkcs15+onepin --use-default-transport-key --label '#{nick}'" then
+if ! run_tool("pkcs15-init",  "--create-pkcs15 --profile pkcs15+onepin --use-default-transport-key --label '#{nick}'") then
 	STDERR.puts "Error running pkcs15-init --create-pkcs15"
 	exit 3
 end
@@ -35,14 +50,14 @@ while keysize != '1024' && keysize != '2048' do
 	print "What is your preferred RSA key size (1024k or 2048k)? "
 	keysize = STDIN.readline.chomp
 end
-if ! system "pkcs15-init --generate-key rsa/#{keysize} --auth-id 01" then
+if ! run_tool("pkcs15-init", "--generate-key rsa/#{keysize} --auth-id 01") then
 	STDERR.puts "Error running pkcs15-init --generate-key"
 	exit 4
 end
 
 # create revocation blob
 tf = Tempfile.new 'revo-blob-signed'
-revo_blob = `pkcs15-crypt -s -i #{File.dirname(File.expand_path $0)}/revocation.txt -o #{tf.path} --pkcs1 --sha-256`
+revo_blob = run_tool("pkcs15-crypt", "-s -i #{File.dirname(File.expand_path $0)}/revocation.txt -o #{tf.path} --pkcs1 --sha-256", true)
 if $? != 0 then
 	STDERR.puts "Error creating revocation blob."
 	exit 5
@@ -52,9 +67,9 @@ puts Base64.encode64(tf.read)
 system "wipe -f -i #{tf.path}"
 
 # store key in git repository and push it to origin
-keys = `pkcs15-tool -k`
+keys = run_tool("pkcs15-tool", "-k", true)
 key_id = keys[/ID          : ([a-f0-9]+)/, 1]
-key_der = `pkcs15-tool --read-public-key #{key_id} | openssl rsa -pubin -inform PEM -outform DER`
+key_der = run_tool("pkcs15-tool",  "--read-public-key #{key_id} | openssl rsa -pubin -inform PEM -outform DER", true)
 if key_der.size == 0 then
 	STDERR.puts "Error reading public key from card."
 	exit 6
