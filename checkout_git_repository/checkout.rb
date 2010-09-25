66e45b2fb2d5e364d61ae98ea120f33076fe3547
Alexander Klink <git@alech.de>
2010-09-25 13:41:16 +0200

    formatting: tabs instead of spaces

--- checkout_git_repository/checkout.rb
+++ checkout_git_repository/checkout.rb
@@ -17,12 +17,12 @@ DEBUG = false
 # and clones it to its final place.
 
 def valid_signature(tag)
-    system "git-verify-tag #{tag}"
+	system "git-verify-tag #{tag}"
 end
 
 def signer_id(tag)
-    result = `git-verify-tag #{tag} 2>&1`
-    result[/key ID ([0-9A-F]{8})/, 1]
+	result = `git-verify-tag #{tag} 2>&1`
+	result[/key ID ([0-9A-F]{8})/, 1]
 end
 
 ENV['GNUPGHOME'] = '/tmp' # FIXME: dir from config with board member keyring
@@ -38,32 +38,32 @@ most_current_signed_object = nil
 pp g.tags if DEBUG
 # iterate over all tags, as they are not sorted by time
 g.tags.each do |tag|
-    puts "Checking tag #{tag.name}" if DEBUG
-    if ! tag.name[/^[a-zA-Z0-9]+$/] then
-        puts 'Argh, non-alphanumeric tag name, WTF?'
-        exit 1
-    end
-    object_id = tag.contents_array[0][/object ([a-f0-9]+)/, 1]
-    puts "Refers to object id #{object_id}" if DEBUG
-    if valid_signature(tag.name) then
-        puts "Valid signature on #{tag.name} by #{signer_id(tag.name)}" if DEBUG
-        object_signatures[object_id] ||= {}
-        object_signatures[object_id][signer_id(tag.name)] = 1
-    end
+	puts "Checking tag #{tag.name}" if DEBUG
+	if ! tag.name[/^[a-zA-Z0-9]+$/] then
+		STDERR.puts 'Argh, non-alphanumeric tag name, WTF?'
+		next
+	end
+	object_id = tag.contents_array[0][/object ([a-f0-9]+)/, 1]
+	puts "Refers to object id #{object_id}" if DEBUG
+	if valid_signature(tag.name) then
+		puts "Valid signature on #{tag.name} by #{signer_id(tag.name)}" if DEBUG
+		object_signatures[object_id] ||= {}
+		object_signatures[object_id][signer_id(tag.name)] = 1
+	end
 end
 pp object_signatures if DEBUG
 
 # iterate over all commits to find the first that has more than one signature
 g.log.each do |log|
-    if object_signatures[log.objectish] && object_signatures[log.objectish].keys.size >= 2 then
-        most_current_signed_object = log.objectish
-        break
-    end
+	if object_signatures[log.objectish] && object_signatures[log.objectish].keys.size >= 2 then
+		most_current_signed_object = log.objectish
+		break
+	end
 end
 
 if ! most_current_signed_object then
-    STDERR.puts "Sorry, no object with more than one signed tag found."
-    exit 1
+	STDERR.puts "Sorry, no object with more than one signed tag found."
+	exit 1
 end
 
 g.reset_hard(most_current_signed_object)
--- door/check.rb
+++ door/check.rb
@@ -15,43 +15,42 @@ keys = []
 key_lengths = []
 keys_result = `pkcs15-tool --list-keys -w`
 keys_result.split(/\n\n/).each do |key|
-    pp key if DEBUG
-    key.split(/\n/).each do |line|
-        if line[/Usage/] && ! line[/sign/] then
-            next # not used for signing, skip
-        end
-        # FIXME
-        # those access flags are just educated guesses for
-        # now, research that they mean what I think they do
-        if line[/Access Flags/] && \
-            (! line[/alwaysSensitive/] || ! line[/neverExtract/] \
-             || ! line[/local/]) then
-             next # access flags are weird, skip
-        end
-        if line[/ModLength/] then
-            if (! line[/1024/] || !line[/2048/]) then
-                next # unsupported key length
-            else
-                key_lengths << line[/(\d+)/, 1]
-            end
-        end
-        if key_id = line[/ID          : ([0-9a-f]+)/, 1] then
-            keys << key_id
-
-        end
-    end
+	pp key if DEBUG
+	key.split(/\n/).each do |line|
+		if line[/Usage/] && ! line[/sign/] then
+			next # not used for signing, skip
+		end
+		# FIXME
+		# those access flags are just educated guesses for
+		# now, research that they mean what I think they do
+		if line[/Access Flags/] && \
+			(! line[/alwaysSensitive/] || ! line[/neverExtract/] \
+			 || ! line[/local/]) then
+			 next # access flags are weird, skip
+		end
+		if line[/ModLength/] then
+			if (! line[/1024/] || !line[/2048/]) then
+				next # unsupported key length
+			else
+				key_lengths << line[/(\d+)/, 1]
+			end
+		end
+		if key_id = line[/ID		  : ([0-9a-f]+)/, 1] then
+			keys << key_id
+		end
+	end
 end
 
 pp key_lengths if DEBUG
 
 # get DER-encoded public keys
 keys_der = keys.map do |k|
-    `pkcs15-tool --read-public-key #{k} | openssl rsa -inform pem -outform der -pubin`
+	`pkcs15-tool --read-public-key #{k} | openssl rsa -inform pem -outform der -pubin`
 end
 
 # compute git hash on the keys
 key_hashes = keys_der.map do |k|
-     Digest::SHA1.hexdigest('blob ' + k.size.to_s + "\0" + k)
+	 Digest::SHA1.hexdigest('blob ' + k.size.to_s + "\0" + k)
 end
 pp key_hashes if DEBUG
 
@@ -59,32 +58,32 @@ pp key_hashes if DEBUG
 g = Git.open(OpenCorn::Config['ACCEPTED_SIGNED_REPO'])
 
 key_in_repo = nil
-key_file    = nil
+key_file    =  nil
 key_length  = nil
 key_hash    = nil
 key_hashes.each_with_index do |hash, index|
-    # find blobs in HEAD that have the correct git hash
-    if blob = g.object('HEAD').gtree.blobs.to_a.find { |entry| entry[1].objectish == hash } then
-        key_file    = blob[0]
-        key_in_repo = keys[index]
-        key_length  = key_lengths[index]
-        key_hash    = key_hashes[index]
-        break
-    end
+	# find blobs in HEAD that have the correct git hash
+	if blob = g.object('HEAD').gtree.blobs.to_a.find { |entry| entry[1].objectish == hash } then
+		key_file    = blob[0]
+		key_in_repo = keys[index]
+		key_length  = key_lengths[index]
+		key_hash    = key_hashes[index]
+		break
+	end
 end
 
 if ! key_in_repo then
-    # FIXME: log attempt
-    STDERR.puts "Sorry, no usable keys found on card."
-    exit 1
+	# FIXME: log attempt
+	STDERR.puts "Sorry, no usable keys found on card."
+	exit 1
 end
 
 puts key_in_repo if DEBUG
 
 g_r = Git.open(OpenCorn::Config['REVOCATION_REPO'])
 if g_r.object('HEAD').gtree.blobs.to_a.find { |entry| entry[1].objectish == key_hash } then
-    STDERR.puts "Sorry, key has been revoked."
-    exit 2
+	STDERR.puts "Sorry, key has been revoked."
+	exit 2
 end
 
 # let the user sign a challenge
@@ -100,15 +99,15 @@ system "pkcs15-crypt -k #{key_in_repo} -s -i #{signer_file.path} -o #{signature_
 # verify signature
 sig_result = `openssl rsautl -verify -in #{signature_file.path} -inkey #{REPO}/#{key_file} -keyform DER -pubin -raw`[-32,32]
 if ! $?.success? then
-    # FIXME: log
-    STDERR.puts "Invalid signature, sorry"
-    exit 3
+	# FIXME: log
+	STDERR.puts "Invalid signature, sorry"
+	exit 3
 end
 
 if sig_result != challenge then
-    # FIXME: log
-    STDERR.puts "Signature does not match challenge."
-    exit 4
+	# FIXME: log
+	STDERR.puts "Signature does not match challenge."
+	exit 4
 end
 
 # TODO: call command to open door
