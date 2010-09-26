8f2a3722355446446b8d49e1b5c584a98c89bb08
Alexander Klink <git@alech.de>
2010-09-26 16:18:29 +0200

    secret shared password recovery

---
+++ recover_log_password/recover_password.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'opencorn/config'
+require 'secretsharing'
+
+k = OpenCorn::Config['SECRETSHARING_K']
+s = SecretSharing::Shamir.new(k)
+
+(1..k).each do |i|
+	puts "Please enter share number #{i}:"
+	s << STDIN.readline.chomp
+	puts "\e[H\e[2J"
+end
+
+puts "The password is: #{s.secret_password}"
