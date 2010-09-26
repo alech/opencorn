4cdb0e954f79861514e04c037c4712a403c77da0
Alexander Klink <git@alech.de>
2010-09-26 16:57:21 +0200

    Bugfix for revoke.rb (refactoring was untested ...)

--- revoke/revoke.rb
+++ revoke/revoke.rb
@@ -12,7 +12,7 @@ require 'git'
 # empty password for self-revocation.
 
 
-def read_and_check_hashcash
+def read_and_check_hashcash(resource, required_bits)
 	stamp = STDIN.readline.chomp
 	s = begin
 			HashCash::Stamp.new(:stamp => stamp)
@@ -31,7 +31,8 @@ def read_and_check_hashcash
 	end
 end
 
-resource = "opencorn-" + Base64.encode64(OpenSSL::Random.random_bytes(9)).chomp.downcase
+resource = "opencorn-" + Base64.encode64(OpenSSL::Random.random_bytes(9)). \
+                         chomp.downcase
 required_bits = OpenCorn::Config['HASHCASH_BITS'] || 24
 
 # TODO: JavaScript implementation and link to it.
@@ -43,9 +44,10 @@ Please enter a hash cash stamp for the resource '#{resource}'
 with a value of #{required_bits} bits.
 XEOF
 
-read_and_check_hashcash
+read_and_check_hashcash(resource, required_bits)
 
-puts "Thanks, now please paste your revocation blob, followed by an empty line."
+puts "Thanks, now please paste your revocation blob, " \
+     "followed by an empty line."
 revo_blob = ""
 while (line = STDIN.readline) != "\n" do
 	revo_blob += line
@@ -61,24 +63,30 @@ g = Git.open(OpenCorn::Config['ACCEPTED_REPO'])
 
 key_file_to_revoke = nil
 g.ls_files.keys.each do |key_file|
-	puts "running openssl rsautl -verify -in #{signature_file.path} -inkey #{OpenCorn::Config['ACCEPTED_REPO']}/#{key_file} -keyform DER -pubin"
-	sig_result = `openssl rsautl -verify -in #{signature_file.path} -inkey #{OpenCorn::Config['ACCEPTED_REPO']}/#{key_file} -keyform DER -pubin`[-32,32]
+	puts "running openssl rsautl -verify -in #{signature_file.path} " \
+	     "-inkey #{OpenCorn::Config['ACCEPTED_REPO']}/#{key_file} " \
+	     "-keyform DER -pubin"
+	sig_result = `openssl rsautl -verify -in #{signature_file.path} \
+	              -inkey #{OpenCorn::Config['ACCEPTED_REPO']}/#{key_file} \
+	              -keyform DER -pubin`[-32,32]
 	next if ($? != 0)
-	if sig_result == File.read(OpenCorn::Config['ETC'] + "/revocation.txt") then
+	if sig_result == File.read("#{OpenCorn::Config['ETC']}/revocation.txt") then
 		key_file_to_revoke = key_file
 		break
 	end
 end
 
 if ! key_file_to_revoke then
-	STDERR.puts "Sorry, no file to revoke found (maybe your revocation blob is wrong?"
+	STDERR.puts "Sorry, no file to revoke found " \
+	            "(maybe your revocation blob is wrong?"
 	exit 3
 end
 
 g2 = Git.open(OpenCorn::Config['REVOCATION_REPO'])
 g2.pull
 
-File.copy(OpenCorn::Config['ACCEPTED_REPO'] + "/#{key_file_to_revoke}", OpenCorn::Config['REVOCATION_REPO'])
+File.copy(OpenCorn::Config['ACCEPTED_REPO'] + "/#{key_file_to_revoke}", \
+          OpenCorn::Config['REVOCATION_REPO'])
 g2.add key_file_to_revoke
 g2.commit "[revoke] Self-revocation for #{key_file_to_revoke}"
 g2.push
