17da717797fbf9f43d6b0f57c6573ac09159c47f
Julian Wälde <jwaelde@cdc.informatik.tu-darmstadt.de>
2010-10-02 18:05:36 +0000

    first shot on the loggin facility

---
+++ log/log.rb
@@ -0,0 +1,56 @@
+require 'openssl'
+require 'base64'
+require 'opencorn/config'
+require 'gpgme'
+require 'singleton'
+
+
+module OpenCorn
+  class Log
+    include Singleton
+    @seed = nil
+
+    def initialize
+      @seed=nil
+      puts "OpenCorn::Log initialized"
+    end
+
+    def prng(seed)
+      ctx = Digest::SHA512
+      [ctx.digest("0"+seed),ctx.digest("1"+seed)]
+    end
+
+    def writeLog(msg)
+      fp = File.open(OpenCorn::Config['LOG_FILE'],"a")
+      fp.write(msg)
+      fp.close
+    end
+
+    def getRand()
+        if not @seed then
+          @seed = OpenSSL::Random::random_bytes(128)
+          gpgdata = GPGME::Data.from_str(@seed)
+          gpgkeys = GPGME.list_keys(OpenCorn::Config['GPG_LOG_KEYID'])
+          raise "Too many keys for this KeyID" unless gpgkeys.size == 1
+          gpgout = GPGME.encrypt(gpgkeys,gpgdata)
+          #preceding 00 01 indicates a new seed for the prng encrypted with
+          writeLog("\x00\x01"+Base64::encode64(gpgout))
+        end
+
+        @seed,out = prng(@seed)
+        out
+    end
+
+    def cryptMsg(msg)
+        myrand = getRand()
+        ctx = OpenSSL::Cipher::AES256.new("OFB")
+        ctx.iv  = myrand[0..15]  #128bit iv
+        ctx.key = myrand[16..47] #256bit key
+        ctext = ctx.update(msg)+ctx.final()
+        out = OpenSSL::HMAC.digest("SHA1",myrand[48..63],ctext)+ctext 
+        #preceding 00 indicates that the base64 string is as symmetric encrypted logentry
+        writeLog("\x00"+Base64::encode64(out))
+    end
+    alias << cryptMsg
+  end
+end
